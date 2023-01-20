a16d93277980f96502fefadea261520a45260f91
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2023-01-20 16:01:30 -0300

    updating folder of question and making elements dynamic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,22 @@
-import { useContext } from 'react';
-import { QuizContext } from '../context/quiz';
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
 
 const Questions = () => {
   const [quizState, dispatch] = useContext(QuizContext);
-
-  console.log(quizState);
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
 
   return (
-    <div>Questions</div>
-  )
-}
+    <div id="question">
+      <p>
+        Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length} 
+      </p>
+      <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        <p>Opções</p>
+      </div>
+      <button>Continuar</button>
+    </div>
+  );
+};
 
-export default Questions
+export default Questions;
