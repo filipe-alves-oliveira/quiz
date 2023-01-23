d8e75d2bbdcdfd7f3ff4687faac4387ef4d75d3c
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2023-01-23 16:56:50 -0300

    View question options

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 import "./Question.css"
+import Option from "./Option";
 
 const Questions = () => {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -13,7 +14,9 @@ const Questions = () => {
       </p>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+          <Option option={option} key={option}/>
+        ))}
       </div>
       <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
     </div>
